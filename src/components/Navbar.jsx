a978f70f967c54ea7780e8258aa7ff8b371edd3d
ganesh <ganeshsadhashivam@outlook.com>
2023-02-07 15:35:53 +0530

    navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  Menu as MenuIcon,
+  Search,
+  SettingsOutlined,
+  ArrowDropDownOutlined,
+} from "@mui/icons-material";
+
+import Flexbetween from "./FlexBetween";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
+import profileImage from "assets/toa-heftiba.jpg";
+import {
+  AppBar,
+  IconButton,
+  InputBase,
+  Toolbar,
+  useTheme,
+} from "@mui/material";
+
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const theme = useTheme();
+  return (
+    <AppBar
+      sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+      }}
+    >
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        {/* Left side */}
+        <Flexbetween>
+          <IconButton onClick={() => console.log("open close sidebar")}>
+            <MenuIcon />
+          </IconButton>
+          <Flexbetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+          </Flexbetween>
+        </Flexbetween>
+        {/* Right side */}
+        <Flexbetween gap="1.5rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
+          </IconButton>
+        </Flexbetween>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
