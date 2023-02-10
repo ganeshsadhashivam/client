e5b02db4ff5a1b8cb57d3866a0242679df462a1e
ganesh <ganeshsadhashivam@outlook.com>
2023-02-10 18:28:39 +0530

    products

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import Layout from "scenes/layout";
 import Dashboard from "scenes/dashboard";
+import Products from "scenes/products";
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
@@ -19,6 +20,7 @@ function App() {
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
             </Route>
           </Routes>
         </ThemeProvider>
